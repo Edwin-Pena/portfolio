5244ee78a286593f4c0f7778ffeeb2b3b943d247
Edwin Peña <edwinsp095@gmail.com>
2024-12-27 19:18:05 -0500

    chevron position adjustment

--- src/components/IntroLogo/ChevronDown.tsx
+++ src/components/IntroLogo/ChevronDown.tsx
@@ -4,23 +4,25 @@ interface Props {
 
 const ChevronDown: React.FC<Props> = ({ className }) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      className={`bi bi-chevron-double-down animate-bounce position-absolute ${
-        className || ""
-      }`.trim()}
-      viewBox="0 0 16 16"
-    >
-      <path
-        fillRule="evenodd"
-        d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
-      />
-      <path
-        fillRule="evenodd"
-        d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
-      />
-    </svg>
+    <div className="chevron-container">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        className={`bi bi-chevron-double-down animate-bounce ${
+          className || ""
+        }`.trim()}
+        viewBox="0 0 16 16"
+      >
+        <path
+          fillRule="evenodd"
+          d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+        />
+        <path
+          fillRule="evenodd"
+          d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+        />
+      </svg>
+    </div>
   );
 };
 
