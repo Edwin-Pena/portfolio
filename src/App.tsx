1dd8c7c1fc07ff02eb21e48a1f0cba3f7186d291
Edwin Peña <edwinsp095@gmail.com>
2025-05-07 15:21:53 -0500

    Add Node and Express to the stack section

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer/Footer";
 
 const App: React.FC = () => {
   const aboutDesc: string =
-    "Hi, I'm Edwin Peña. I'm a frontend developer and industrial designer. Since I was very young, I've been passionate about technology and knowing how things work and why they were created that way. This thought was what led me to immerse myself in the world of development and industrial design. I love working in a team and learning from the people around me and the new challenges I face. I want to learn more about the world of programming, so I strive to learn and improve my skills every day.";
+    "Hi, I'm Edwin Peña a FullStack developer. Since I was very young, I've been passionate about technology and knowing how things work and why they were created that way. This thought was what led me to immerse myself in the world of development. I love working in a team and learning from the people around me and the new challenges I face. I want to learn more about the world of programming, so I strive to learn and improve my skills every day.";
   return (
     <>
       <main>
