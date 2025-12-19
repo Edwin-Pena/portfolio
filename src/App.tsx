d78b020eed72c81278b1053fb3b023fc5af3bfda
Edwin Peña <edwinsp095@gmail.com>
2025-12-19 17:02:40 -0500

    feat: add new skills and update about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import AboutMe from "./components/AboutMe/AboutMe";
-import LogoIntro from "./components/IntroLogo/IntroLogo";
-import Projects from "./components/Projects/Projects";
-import Stack from "./components/Stack/Stack";
-import Footer from "./components/Footer/Footer";
+import AboutMe from './components/AboutMe/AboutMe';
+import LogoIntro from './components/IntroLogo/IntroLogo';
+import Projects from './components/Projects/Projects';
+import Stack from './components/Stack/Stack';
+import Footer from './components/Footer/Footer';
 
 const App: React.FC = () => {
   const aboutDesc: string =
-    "Hi, I'm Edwin Peña a FullStack developer. Since I was very young, I've been passionate about technology and knowing how things work and why they were created that way. This thought was what led me to immerse myself in the world of development. I love working in a team and learning from the people around me and the new challenges I face. I want to learn more about the world of programming, so I strive to learn and improve my skills every day.";
+    "Hi, I'm Edwin Peña a Software Developer. Since I was very young, I've been passionate about technology and knowing how things work and why they were created that way. This thought was what led me to immerse myself in the world of development. I love working in a team and learning from the people around me and the new challenges I face. I want to learn more about the world of programming, so I strive to learn and improve my skills every day.";
   return (
     <>
       <main>
-        <LogoIntro name="Edwin Peña" />
+        <LogoIntro name='Edwin Peña' />
         <AboutMe description={aboutDesc} />
         <Projects />
         <Stack />
